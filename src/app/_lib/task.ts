191699c4771be22d9cbf8771012f44e076a657cc
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2024-10-29 15:17:40 +0300

    Added dynamic data to tasks table

--- src/app/_lib/task.ts
+++ src/app/_lib/task.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import Error from 'next/error';
 
 export interface TaskData {
     title: string;
@@ -6,3 +8,18 @@ export interface TaskData {
     image: File | null; 
     state: string
 }
+
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/tasks`;
+
+// get user tasks
+export const getUserTasksService = async () => {    
+    try {
+        const res = await axios.get(`${API_URL}/user`, {withCredentials: true});
+        const userTasks = res.data;         
+        return userTasks;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        console.error("Error getting user tasks:", error);
+        throw new Error(error.response?.data.message || "Get tasks failed"); 
+    }
+};
